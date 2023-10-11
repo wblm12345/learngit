b7976b9e6273657c9022b10dc92ec24c83612d56
KK <kk@KK-Macbook.local>
2023-10-11 22:07:32 +0800

    looking forward to the next holiday

--- main.c
+++ main.c
@@ -4,4 +4,5 @@ int main(int argc, char *argv[]) {
 	printf("Welcome to C language!\n");
 	printf("Happy Mid-Autumn Festival!\n");
         printf("Ready to go to school!\n");
+		printf("Looking forward to the next holiday!\n");
 }
